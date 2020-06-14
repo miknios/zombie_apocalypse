ef1f0fae1c2aa9a06b646315f0ea35e1507e1e03
miknios <miknios@gmail.com>
2020-06-14 22:07:58 +0200

    file structure

---
+++ Assets/Scripts/AuthoringComponents/GunAuthoring.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using Unity.Entities;
+using UnityEngine;
+
+namespace ECS_Logic.Common.Collision.Components
+{
+	public class GunAuthoring : MonoBehaviour, IConvertGameObjectToEntity, IDeclareReferencedPrefabs
+	{
+		[SerializeField] private GameObject projectilePrefab;
+		[SerializeField] private KeyCode keyToTrigger;
+
+		public void DeclareReferencedPrefabs(List<GameObject> referencedPrefabs)
+		{
+			referencedPrefabs.Add(projectilePrefab);
+		}
+		public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
+		{
+			// TODO: create gun entity with parameters and projectile prefab entity 
+			// then think how to assign controls for shooting - set system field, keep keycode as component or maybe blob asset
+			// maybe it's not important right now so let's try setting system field and doing if(input is pressed) -> run shoot job
+			
+		}
+	}
+}
