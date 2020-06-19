c87861c6e7038fb87ba9d16f30f13adbfc3a558d
miknios <miknios@gmail.com>
2020-06-19 19:58:41 +0200

    added initially deactivated protection

---
+++ Assets/InitiallyDeactivatedObjectsDeactivator.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class InitiallyDeactivatedObjectsDeactivator : MonoBehaviour
+{
+	private void Awake()
+	{
+		var objectsToDeactivate = GetComponentsInChildren<InitiallyDeactivated>();
+		foreach (var objectToDeactivate in objectsToDeactivate)
+		{
+			objectToDeactivate.gameObject.SetActive(false);
+		}
+	}
+}
---
+++ Assets/Scripts/GraphicClickDetectionDeactivator.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace DefaultNamespace
+{
+	public class GraphicClickDetectionDeactivator : MonoBehaviour, IPointerDownHandler
+	{
+		[SerializeField] private GameObject objectToDeactivate;
+		
+		public void OnPointerDown(PointerEventData eventData)
+		{
+			objectToDeactivate.SetActive(false);
+		}
+	}
+}
