b9adbe030ccf064e7142d7e7fdf20819432388f6
miknios <miknios@gmail.com>
2020-06-19 21:00:05 +0200

    file structure

---
+++ Assets/Scripts/Common/TimescaleController.cs
@@ -0,0 +1,18 @@
+﻿using Signals;
+using UnityEngine;
+using Zenject;
+
+public class TimescaleController : MonoBehaviour
+{
+	[Inject] private SignalBus signalBus = null;
+
+	private void Awake()
+	{
+		signalBus.Subscribe<GameOverSignal>(() => SetTimescaleToZero());
+	}
+
+	private void SetTimescaleToZero()
+	{
+		Time.timeScale = 0;
+	}
+}
